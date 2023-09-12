adf1000e4f5110c0ea76a091bcd12aaf5c8cd8fe
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-12 15:16:55 +0900

    111

---
+++ Chapter02_연산자/src/자바연산자.java
@@ -0,0 +1,29 @@
+/*
+ * 		연산자 / 피연산자(대상) 
+ * 		10 + 20
+ * 		--   -- => 피연산자
+ * 		피연산자 1개 : 단항연산자
+ * 			   2개 : 이항연산자
+ * 			   3개 : 삼항연산자
+ * 		단항연산자
+ * 			=> 증감연산자 (한개증가, 한개감소) ++,-- (반복문에서 주로 쓰는 형태)
+ * 				int a=10;
+ * 				++a; ==> a=11
+ * 			=> 부정연산자 : boolean만 사용이 가능
+ * 				boolean b=false;
+ * 				b=!b; ==> true (true/false 계속 왔다갔다 => 턴제게임)
+ * 			=> 형변환 연산자 : (int)'A'
+ * 				= UpCasting ================> 작은데이터형을 큰데이터형으로
+ * 					자동 형변환
+ * 				= DownCasting	(int)10.5 ==> 큰데이터형을 작은데이터형으로
+ * 					강제 형변환
+ * 		
+ */
+public class 자바연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
