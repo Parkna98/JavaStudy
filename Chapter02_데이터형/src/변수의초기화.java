fdbbb822e407b938d910c96a05821d77facb9809
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-12 12:35:39 +0900

    3333

---
+++ Chapter02_데이터형/src/변수의초기화.java
@@ -0,0 +1,23 @@
+/*
+ * 		변수의 초기화
+ * 		1) 변수 추출
+ * 		2) 변수 선언 (데이터형)
+ * 		3) 변수 초기화
+ * 			= 명시적인 초기화
+ * 				int a=10;
+ * 			= 입력값 
+ * 				***Scanner : 키보드 입력값을 받을 때 사용
+ * 				BufferedReader : 기능을 동일 (단점;예외처리)
+ * 			= 임의값을 받아서 초기화
+ * 			  ------랜덤
+ * 			= 크롤링 
+ * 
+ */
+public class 변수의초기화 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
