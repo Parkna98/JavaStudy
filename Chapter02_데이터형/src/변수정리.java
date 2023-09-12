db6f2d358c84e3a0b68a59c1752e573427be92d9
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-12 14:04:32 +0900

    4444

---
+++ Chapter02_데이터형/src/변수정리.java
@@ -0,0 +1,18 @@
+/*
+ * 		자바 => 형식
+ * 		1) 변수 : 한개의 데이터를 저장할 메모리 공간의 이름
+ * 				------------
+ * 			변수 선언
+ * 			 데이터형 변수명; (자바) => 변수 데이터형 (오라클)
+ * 			-------   ㅣ	
+ * 			   ㅣ	  ㅣ
+ * 			변수 초기값 
+ */
+public class 변수정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
