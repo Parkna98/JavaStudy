20eaefdde9c19407d3df074f994ef0ea155dff0d
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-13 14:04:53 +0900

    11

---
+++ Chapter02_연산자2/src/비교연산자.java
@@ -0,0 +1,21 @@
+/*
+ * 		44page
+ * 		비교연산자 ===> 결과값 (boolean : true/false)
+ * 		--------사용처 (조건문, 반복문)
+ * 		숫자 (정수, 실수, boolean, 문자) => 문자열은 비교할 수 없다
+ * 		---------------------------
+ * 		1) == 같다
+ * 		2) =! 같지않다
+ * 		3) <  작다
+ * 		4) >  크다
+ * 		5) <= 작거나 같다
+ * 		6) >= 크거나 같다
+ */
+public class 비교연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
