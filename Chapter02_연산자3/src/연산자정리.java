292fd76acf65e1d60bcedc7d0d8649e30b098571
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-14 16:00:22 +0900

    1111

--- Chapter02_연산자3/src/연산자정리.java
+++ Chapter02_연산자3/src/연산자정리.java
@@ -1,6 +1,17 @@
 /*
  * 		연산자
  * 		----- 메소드(기능수행)
+ * 		증감 연산자 : 반복문에 주로 사용 (++,--)
+ * 		부정 연산자 : 반대효과
+ * 		형변환 연산자 : 클래스 형변환
+ * 		--------------------------------
+ * 		산술 : +(문자열 결합) 
+ * 			  %(년도 계산, 페이지)
+ * 		비교연산자 : 반복문, 조건문
+ * 		논리연산자 : 기간, 범위 ==> &&
+ * 		대입연산자 : =, +=
+ * 		---------------------------------
+ * 		
  * 		
  */
 // 외부라이브러리 갖고오기 자바폴더 우클릭 - Build Path - configure path - class path 선택 - add external - apply
@@ -22,7 +33,7 @@ public class 연산자정리 {
 		Elements singer=doc.select("a.artist");
 		for(int i=0;i<50;i++)
 		{
-			if(title.get(i).text().contains(fd)) 
+			if(singer.get(i).text().contains(fd)) 
 			{
 			System.out.println(title.get(i).text()
 					+ "("+singer.get(i).text()+")"
