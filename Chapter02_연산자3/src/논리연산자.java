806a8487d9d4baecc3b30fe89e1d313e9a5724e6
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-14 12:49:23 +0900

    111

--- Chapter02_연산자3/src/논리연산자.java
+++ Chapter02_연산자3/src/논리연산자.java
@@ -18,12 +18,31 @@
  *			true		false	=> true
  *			false		true	=> true
  *			false 		false 	=> false
+ *
  */
+//		||
+/*
+ * 		1~12 입력 =>
+ * 		12,1,2 입력 => 겨울
+ * 		3,4,5 ==> 봄
+ * 		6,7,8 ==> 여름
+ * 		9,10,11 ==> 가을
+ */
+import java.util.Scanner;
+
 public class 논리연산자 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		System.out.println("월 입력(1~12):");
+		int month=scan.nextInt();
+		System.out.print(month+"월은 ");
+		System.out.print(month>=3 && month<=5?"봄입니다":"");
+		System.out.print(month>=6 && month<=8?"여름입니다":"");
+		System.out.print(month>=9 && month<=11?"가을입니다":"");
+		System.out.print(month==12 || month==1 || month==2?"겨울입니다":"");
+		
 	}
 
 }
