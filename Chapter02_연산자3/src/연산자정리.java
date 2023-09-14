bd67de51fb4497a47dc35a72dd485f7859406d12
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-14 15:30:56 +0900

    11

--- Chapter02_연산자3/src/연산자정리.java
+++ Chapter02_연산자3/src/연산자정리.java
@@ -4,12 +4,22 @@
  * 		
  */
 // 외부라이브러리 갖고오기 자바폴더 우클릭 - Build Path - configure path - class path 선택 - add external - apply
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 public class 연산자정리 {
 
 	public static void main(String[] args) throws Exception 
 	{
 		// TODO Auto-generated method stub
-
+		Document doc=Jsoup.connect("https://www.genie.co.kr/chart/top200").get();
+		Elements title=doc.select("a.title");
+		Elements singer=doc.select("a.artist");
+		for(int i=0;i<50;i++)
+		{
+			System.out.print(title.get(i).text());
+			System.out.println(" "+singer.get(i).text());
+		}
 	}
 
 }
