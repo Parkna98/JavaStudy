fbfc16b8fe386f34c33adb4c86d09f31412eb308
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-14 14:01:47 +0900

    11

--- Chapter02_연산자3/src/단항연산자.java
+++ Chapter02_연산자3/src/단항연산자.java
@@ -20,28 +20,33 @@ public class 단항연산자 {
 		/*
 		 * int b=a++ - ++a; System.out.println("a="+a); System.out.println("b="+b);
 		 */ 
-		/*
-		 * System.out.println("===== 부정연산자 ====="); boolean bCheck=true;
-		 * System.out.println("변경전:"+bCheck); bCheck=!bCheck;
-		 * System.out.println("변경후:"+bCheck); boolean aa=true; while(true) // 2명이서는
-		 * true/false로 번갈아가기, 3명이상이면 %활용해서 번갈아가기 { aa=!aa; if(aa==true) {
-		 * System.out.println("사용자"); } else { System.out.println("컴퓨터"); } break; }
-		 */
-		int a=1;
-		while(a==3);
-			{ a++;
-				if(a%3==1) {
-					System.out.println("플레이어1");
-					
-				}
-				if(a%3==2) {
-					System.out.println("플레이어2");
-				}
-				else {
-					System.out.println("플레이어3");
-				}
-			
-			}
-	}
-
-}
+		
+//		  System.out.println("===== 부정연산자 ====="); 
+//		  boolean bCheck=true;
+//		  System.out.println("변경전:"+bCheck); 
+//		  bCheck=!bCheck;
+//		  System.out.println("변경후:"+bCheck); 
+//		  boolean aa=true; 
+//		  while(true) // 2명이서는 true/false로 번갈아가기, 3명이상이면 %활용해서 번갈아가기 
+//		  { aa=!aa; if(aa==true) {
+//		  System.out.println("사용자"); } 
+//		  else { System.out.println("컴퓨터"); } break; }
+//		 
+//		int a=1;
+//		while(true);
+//			{ a++;
+//				if(a%3==1) {
+//					System.out.println("플레이어1");
+//					
+//				}
+//				if(a%3==2) {
+//					System.out.println("플레이어2");
+//				}
+//				else {
+//					System.out.println("플레이어3");
+//				}
+//			
+//			}
+//	}
+//
+//}
