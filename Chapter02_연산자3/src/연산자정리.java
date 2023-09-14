975e04a0a0a5ae6907fd37b082ab01d8244a081e
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-14 15:39:22 +0900

    111

--- Chapter02_연산자3/src/연산자정리.java
+++ Chapter02_연산자3/src/연산자정리.java
@@ -7,19 +7,29 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import java.util.*;
 public class 연산자정리 {
 
 	public static void main(String[] args) throws Exception 
 	{
 		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("검색어 입력:");
+		String fd=scan.next();
+		
 		Document doc=Jsoup.connect("https://www.genie.co.kr/chart/top200").get();
 		Elements title=doc.select("a.title");
 		Elements singer=doc.select("a.artist");
 		for(int i=0;i<50;i++)
 		{
-			System.out.print(title.get(i).text());
-			System.out.println(" "+singer.get(i).text());
+			if(title.get(i).text().contains(fd)) 
+			{
+			System.out.println(title.get(i).text()
+					+ "("+singer.get(i).text()+")"
+					);
+			}
 		}
+		
 	}
 
 }
