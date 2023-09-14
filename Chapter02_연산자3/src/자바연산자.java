a3739089dc5fceecc5d7e2551acaf924a6a67b88
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-14 09:30:56 +0900

    1

---
+++ Chapter02_연산자3/src/자바연산자.java
@@ -0,0 +1,37 @@
+/*
+ * 		변수	: 저장공간
+ * 		====
+ * 			자바 저장 공간을 설정 (자료형,데이터형)
+ * 			----------------
+ * 			정수
+ * 			 byte (8bit) => 0,1을 8개 채워서 숫자하나 저장 -128~127
+ * 		 	 int  (32bit) => -21억 4천 ~ 21억 4천
+ * 			 ------------------------default (컴퓨터에서 모든 숫자는 int를 인식)
+ * 			 long (64bit) => 금융권, 빅데이터
+ * 			실수
+ * 			 double (64bit) => 소수점 15자리 이상
+ * 			문자
+ * 			 char (16bit) => 0~65535 (문자번호, 아스키코드, 유니코드)
+ * 					'A' ==> 65
+ * 			논리
+ * 			 boolean : true, false (8bit)
+ * 			-----------------------------------
+ * 			정수 3개 저장
+ * 			int a, b, c;
+ * 			실수 2개 저장
+ * 			double d1, d2
+ * 			문자 1개
+ * 			char c;
+ * 			boolean bb;
+ * 
+ * 		
+ * 		
+ */
+public class 자바연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
