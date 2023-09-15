db0658b07801b69c0d913fa00459f24e9cc6d6c8
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-15 12:50:20 +0900

    111

---
+++ Chapter03_제어문/src/단일조건문_1.java
@@ -0,0 +1,18 @@
+//	알파벳을 받아서 대문자, 소문자, 숫자, 다른문자
+import java.util.Scanner;
+public class 단일조건문_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		// 알파벳 받기
+		// 대문자
+		// 소문자
+		// 숫자
+		// 기타
+		System.out.println("알파벳 입력:");
+		char ch=scan.next().charAt(0);
+
+	}
+
+}
