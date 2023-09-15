0764cdc28821204251d562df0dbe587fe3d7e368
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-15 14:08:20 +0900

    11

--- Chapter03_제어문/src/단일조건문_1.java
+++ Chapter03_제어문/src/단일조건문_1.java
@@ -10,9 +10,25 @@ public class 단일조건문_1 {
 		// 소문자
 		// 숫자
 		// 기타
-		System.out.println("알파벳 입력:");
+		System.out.print("알파벳 입력:");
 		char ch=scan.next().charAt(0);
-
+		
+		if(ch>=65 && ch<=90)
+		{
+			System.out.println(ch+"는(은) 대문자입니다");
+		}
+		if(ch>=97 && ch<=122)
+		{
+			System.out.println(ch+"는(은) 소문자입니다");
+		}
+		if(ch>=48 && ch<=57)
+		{
+			System.out.println(ch+"는(은) 숫자입니다");
+		}
+		if(!((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch>=48 && ch<=57)))
+		{
+			System.out.println(ch+"는(은) 기타입니다.");
+		}
 	}
 
 }
