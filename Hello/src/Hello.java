2ae854511a31c94e272704033342ab16b8c76020
LG <LG@DESKTOP-CNFEIQL>
2023-09-17 12:01:56 +0900

    111

---
+++ Hello/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world.");
+		System.out.println("/* 주석연습입니다. */");/* 주석연습입니다. */
+		System.out.println();// 주석연습입니다.
+	}
+}
