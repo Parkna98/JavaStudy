004d1cd6cb9ecc9d49fd1185bae84091f29060ad
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-19 14:38:55 +0900

    22

--- Chapter03_제어문(반복문)/src/제어문_반복문_12.java
+++ Chapter03_제어문(반복문)/src/제어문_반복문_12.java
@@ -1,9 +1,25 @@
-
+// 1~30까지 출력 => 30사이의 짝수만 출려
+/*
+ *  2 4 6
+ *  8 10 12 => 3개씩 출력
+ */
 public class 제어문_반복문_12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		for(int i=1;i<=30;i++)
+		{	
+			
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+			if(i%6==0)
+			{
+				System.out.println("\n");
+			}
+		}
 	}
 
 }
