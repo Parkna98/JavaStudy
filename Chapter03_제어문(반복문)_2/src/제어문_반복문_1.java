31f7d619c78fd817375cf6b7077b52c38ed5494f
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-20 10:06:38 +0900

    33

--- Chapter03_제어문(반복문)_2/src/제어문_반복문_1.java
+++ Chapter03_제어문(반복문)_2/src/제어문_반복문_1.java
@@ -85,6 +85,16 @@ public class 제어문_반복문_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 웹에서는 2차 for문은 사용하지 않는다
+		// 코테 => sort, 중복없는 난수 발생....
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=3;j++)
+			{
+				System.out.println("i="+i+",j="+j);
+			}
+			System.out.println();
+		}
 
 	}
 
