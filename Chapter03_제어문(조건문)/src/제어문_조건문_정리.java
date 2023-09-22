147e8473c853cfa89c36239225fc42a648dad510
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-22 16:02:45 +0900

    11

--- Chapter03_제어문(조건문)/src/제어문_조건문_정리.java
+++ Chapter03_제어문(조건문)/src/제어문_조건문_정리.java
@@ -237,10 +237,10 @@ public class 제어문_조건문_정리 {
 					
 				}
 	// 4-6. 두개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의수 출력
-				for(int i=1;i<=6;i++) {
+				for(int i1=1;i1<=6;i1++) {
 					for(int j=1;j<=6;j++) {
-						if(i+j==6) {
-							System.out.printf("[%d,%d]",i,j);
+						if(i1+j==6) {
+							System.out.printf("[%d,%d]",i1,j);
 					}
 					}
 				}
