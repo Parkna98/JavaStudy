63e9dd84dd3fe18e66fd6a1fd71595d9682b950f
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-22 14:02:50 +0900

    11

--- Chapter03_제어문(조건문)/src/제어문_조건문_정리.java
+++ Chapter03_제어문(조건문)/src/제어문_조건문_정리.java
@@ -225,8 +225,9 @@ public class 제어문_조건문_정리 {
 			 *  ===>
 			 */
 				System.out.println();
-				int k=0, t=0;
+				int k=0;
 				while(k<=10) {
+					int t=0;
 					while(t<=k) {
 						System.out.print("*");
 						t++;
@@ -234,6 +235,14 @@ public class 제어문_조건문_정리 {
 					System.out.println();
 					k++;
 					
+				}
+	// 4-6. 두개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의수 출력
+				for(int i=1;i<=6;i++) {
+					for(int j=1;j<=6;j++) {
+						if(i+j==6) {
+							System.out.printf("[%d,%d]",i,j);
+					}
+					}
 				}
 			
 	}
---
+++ Chapter04_배열/src/배열_생성.java
@@ -0,0 +1,18 @@
+/*
+ * 		배열
+ * 		--- 묶어서 관리
+ * 			--------
+ * 			1) 변수 => 같은 유형의 변수가 3개이상 => 묶어서 처리
+ * 											 -----
+ * 											 변수명을 한개로 관리
+ * 			2) 명령문 
+ * 
+ */
+public class 배열_생성 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
