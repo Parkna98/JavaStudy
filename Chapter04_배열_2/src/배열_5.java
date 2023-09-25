e84c4fe0554d67788abc73d8ef768f298aeb1f80
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-25 14:03:52 +0900

    11

---
+++ Chapter04_배열_2/src/배열_5.java
@@ -0,0 +1,26 @@
+/* 
+ * 		간단하게 짜는 중복없는 난수
+ */
+public class 배열_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr=new int[10];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=(int)(Math.random()*10)+1;
+			for(int j=0;j<i;j++) {
+				if(arr[i]==arr[j]) {
+					i--; // 다시 난수 발생 arr[3]이 중복이라면 arr[3]을 다시 발생시켜라
+					// 인덱스를 이용해서 다시 난수 발생
+					break;
+				}
+			}
+		}
+		
+		//출력
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
