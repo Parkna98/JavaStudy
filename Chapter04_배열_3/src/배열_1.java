4e8f6437332b4862e16399a3d949110b55b9fb1a
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-25 14:04:10 +0900

    11

---
+++ Chapter04_배열_3/src/배열_1.java
@@ -0,0 +1,18 @@
+/*
+ * 		1. 86page
+ * 			new => 메모리 확보
+ * 				   -------- 메모리 크기 
+ * 					   ㅣ
+ * 					메모리 주소를 전송
+ * 			예) 
+ * 				
+ * 
+ */
+public class 배열_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
