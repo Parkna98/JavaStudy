defa1cbf2ca1e4205c4eaf3083fe4df66701d0e7
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-25 16:35:52 +0900

    요일

--- Chapter04_배열_3/src/배열_6.java
+++ Chapter04_배열_3/src/배열_6.java
@@ -1,20 +1,40 @@
-// 선택정렬
+// 선택정렬 
+// 앞에 문자부터 고정시킴
 /* 알파벳 ==> 문자열은 안된
  * 			--------- 비교연산자를 사용할 수 없다
  * 			compare() 음수/양수/0 *
- * 			Stirng s="H;
+ * 			String s="H";
  * 			String s1="K"
  * 	
  * 			s.compare(s1)
  * 			-         --
  *		 	H  		  k ==> -2
-  
  */
+import java.util.*;
 public class 배열_6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char[] alpha=new char[10];
+		for(int i=0;i<alpha.length;i++) {
+			alpha[i]=(char)((Math.random()*26)+65);
+		}
+		System.out.println("==== 정렬전 ====");
+		System.out.println(Arrays.toString(alpha));
+		
+		System.out.println("==== 정렬후 ====");
+		for(int i=0;i<alpha.length-1;i++) {
+			for(int j=i+1;j<alpha.length;j++) {
+				if(alpha[i]>alpha[j]) {
+					char temp=alpha[i];
+					alpha[i]=alpha[j];
+					alpha[j]=temp;
+				}
+			}
+			System.out.println((i+1)+"Round==>"+alpha[i]);
+			System.out.println(Arrays.toString(alpha));
+		}
+		System.out.println(Arrays.toString(alpha));
 	}
 
 }
