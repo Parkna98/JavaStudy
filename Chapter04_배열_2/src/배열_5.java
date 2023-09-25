5758b0a828e860a75b1f21f86fc909c1c9276d9a
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-26 08:59:09 +0900

    11

--- Chapter04_배열_2/src/배열_5.java
+++ Chapter04_배열_2/src/배열_5.java
@@ -5,21 +5,36 @@ public class 배열_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		int[] arr=new int[10];
+//		for(int i=0;i<arr.length;i++) {
+//			arr[i]=(int)(Math.random()*10)+1;
+//			for(int j=0;j<i;j++) {
+//				if(arr[i]==arr[j]) {
+//					i--; // 다시 난수 발생 arr[3]이 중복이라면 arr[3]을 다시 발생시켜라
+//					// 인덱스를 이용해서 다시 난수 발생
+//					break;
+//				}
+//			}
+//		}
+//		
+//		//출력
+//		for(int i:arr) {
+//			System.out.print(i+" ");
+//		}
+		
+		// 중복없는 난수
 		int[] arr=new int[10];
 		for(int i=0;i<arr.length;i++) {
 			arr[i]=(int)(Math.random()*10)+1;
-			for(int j=0;j<i;j++) {
+			for(int j=1;j<i;j++) {
 				if(arr[i]==arr[j]) {
-					i--; // 다시 난수 발생 arr[3]이 중복이라면 arr[3]을 다시 발생시켜라
-					// 인덱스를 이용해서 다시 난수 발생
+					i--;
 					break;
 				}
 			}
 		}
-		
-		//출력
-		for(int i:arr) {
-			System.out.print(i+" ");
+		for(int a:arr) {
+			System.out.print(a+" ");
 		}
 	}
 
---
+++ Chapter04_배열_3/src/배열연습.java
@@ -0,0 +1,72 @@
+
+public class 배열연습 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 중복없는 난수 10개 발생
+//		int[] arr=new int[10];
+//		int su=0; // 난수저장
+//		boolean bCheck=false; // 중복확인
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			bCheck=true;
+//			while(bCheck) {
+//				su=(int)(Math.random()*10)+1;
+//				bCheck=false; // 종료조건을 만들어 둬야함 (무한루프피하기위해)
+//				// 중복체크
+//				for(int j=0;j<i;j++) {
+//					if(arr[j]==su) {
+//						bCheck=true; // 중복이라면 while문 다시 수행
+//						break;
+//					}
+//				}
+//			}
+//			arr[i]=su;
+//			
+//		}
+//		for(int a:arr) {
+//			System.out.print(a+" ");
+//		}
+		
+		//중복없는 10개 난수 발생(1~10)
+//		int[] arr=new int[10];
+//		int su=0; // 난수발생
+//		boolean bCheck=false; // 중복체크
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			bCheck=true;
+//			while(bCheck) {
+//				su=(int)(Math.random()*10)+1;
+//				bCheck=false;
+//				for(int j=0;j<i;j++) {
+//					if(arr[j]==su) {
+//						bCheck=true;
+//						break;
+//					}
+//				}
+//			}
+//			arr[i]=su;
+//		}
+//		for(int a:arr) {
+//			System.out.print(a+" ");
+//		}
+		
+		// 간단하게 짜는 중복없는 난수
+		int[] arr=new int[10];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=(int)(Math.random()*10)+1;
+			for(int j=0;j<i;j++) {
+				if(arr[i]==arr[j]) {
+					i--; // 중복되면 1차 for문으로 돌아가게 만듬
+					break; // if문과는 상관없는 break
+				}
+			}
+		}
+		for(int a:arr) {
+			System.out.print(a+" ");
+		}
+		
+		
+	}
+
+}
