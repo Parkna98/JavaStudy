47866441fd4f1cd164c016281dfa48d25f4b31d2
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-25 16:01:40 +0900

    11

---
+++ Chapter04_배열_3/src/배열_6.java
@@ -0,0 +1,20 @@
+// 선택정렬
+/* 알파벳 ==> 문자열은 안된
+ * 			--------- 비교연산자를 사용할 수 없다
+ * 			compare() 음수/양수/0 *
+ * 			Stirng s="H;
+ * 			String s1="K"
+ * 	
+ * 			s.compare(s1)
+ * 			-         --
+ *		 	H  		  k ==> -2
+  
+ */
+public class 배열_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
