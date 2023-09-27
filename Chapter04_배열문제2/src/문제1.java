52238f939f1db6ea3d8ab9cfd2e2f42423d74f98
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-27 17:26:19 +0900

    11

--- Chapter04_배열문제2/src/문제1.java
+++ Chapter04_배열문제2/src/문제1.java
@@ -1,26 +1,50 @@
 // 1. 5명의 학생의 국어점수를 받아서 등수를 출력하는 프로그램 작성
+// index 0번부터 뒤의 것들과 차례로 비교하면서 그것보다 큰값이 있으면 등수 +1씩 더하기
+import java.util.Arrays;
 import java.util.Scanner;
 public class 문제1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		int[] kor=new int[5];
+//		Scanner scan=new Scanner(System.in);
+//		for(int i=0;i<kor.length;i++) {
+//			System.out.println(i+"번째 학생 국어점수 입력:");
+//			kor[i]=scan.nextInt();
+//		}
+//		for(int i:kor) {
+//			System.out.print(i+" ");
+//		}
+//		for(int i=0;i<kor.length-1;i++) {
+//			for(int j=0;j<kor.length-1;j++) {
+//				if(kor[j]>kor[j+1]) {
+//					int temp=kor[j];
+//					kor[j]=kor[j+1];
+//					kor[j+1]=temp;
+//				}
+//			}
+//		}
 		int[] kor=new int[5];
-		Scanner scan=new Scanner(System.in);
+		int[] rank=new int[5];
+		
+		//국어점수 등록
 		for(int i=0;i<kor.length;i++) {
-			System.out.println(i+"번째 학생 국어점수 입력:");
-			kor[i]=scan.nextInt();
+			kor[i]=(int)(Math.random()*50)+50;
 		}
-		for(int i:kor) {
-			System.out.print(i+" ");
-		}
-		for(int i=0;i<kor.length-1;i++) {
-			for(int j=0;j<kor.length-1;j++) {
-				if(kor[j]>kor[j+1]) {
-					int temp=kor[j];
-					kor[j]=kor[j+1];
-					kor[j+1]=temp;
+		System.out.println("국어점수 출력");
+		System.out.println(Arrays.toString(kor));
+		
+		//등수구하기
+		for(int i=0;i<rank.length;i++) {
+			rank[i]=1;
+			for(int j=0;j<kor.length;j++)
+				if(kor[i]<kor[j]) {
+					rank[i]++;
 				}
-			}
+		}
+		// 결과출력
+		for(int i=0;i<kor.length;i++) {
+			System.out.println(kor[i]+" "+rank[i]);
 		}
 	}
 
--- Chapter04_배열문제2/src/문제4.java
+++ Chapter04_배열문제2/src/문제4.java
@@ -1,17 +1,18 @@
 //4. 10개의 난수를 발생하여 배열에 저장한 후에 인덱스가 1인 곳의 값을 출력하시오.
+import java.util.*;
 public class 문제4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr=new int[10];
+		// 초기값
+		// Random r=new Random() => r.nextInt(100)
 		for(int i=0;i<arr.length;i++) {
-			arr[i]=(int)(Math.random()*10)+1;
+			arr[i]=(int)(Math.random()*100)+1;
+			// double random() => 0.0~0.99
 		}
-		for(int i:arr) {
-			System.out.print(i+" ");
-		}
-		System.out.println();
-		System.out.println(arr[1]);
+		System.out.println(Arrays.toString(arr));
+		System.out.println("인덱스1번:"+arr[1]);
 		
 		// 5. 위 문제의 배열중에서 최대값의 인덱스를 출력
 		int max=1;
