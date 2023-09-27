318c3f5183a8c1190b1e555c8d96528b00e2f1d3
SIST <SIST@DESKTOP-0I2K5P5>
2023-09-27 16:59:39 +0900

    11

--- Chapter04_배열문제2/src/문제6_7.java
+++ Chapter04_배열문제2/src/문제6_7.java
@@ -4,7 +4,25 @@ public class 문제6_7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr=new int[10];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=(int)(Math.random()*100)+1;
+		}
+		int sum=0;
+		double avg=0;
+		for(int i:arr) {
+			sum+=i;
+		}
+		avg=sum/10.0;
+		System.out.println("총합:"+sum);
+		System.out.printf("평균:%.2f",avg);
 		
+		System.out.println("\n=======7=======");
+		// 7.
+		for(int i=0;i<arr.length;i++) {
+			if(i==3) continue;
+			System.out.print(arr[i]+" ");
+		}
 	}
 
 }
