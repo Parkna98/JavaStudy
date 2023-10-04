7c6bf5169dcebefbf51641f21d0b1130897c6a13
SIST <SIST@DESKTOP-0I2K5P5>
2023-10-04 12:08:31 +0900

    11

--- Chapter04_메소드_1/src/메소드_4_구현.java
+++ Chapter04_메소드_1/src/메소드_4_구현.java
@@ -1,9 +1,52 @@
-
+// 메소드_4_구현 
+/*	
+ * 		리턴형 / 매개변수 / 메소드명 => 가독성있게...
+ * 		첫번째기능 => 단입력
+ * 		두번째기능 => 구구단출력
+ */
+import java.util.Scanner;
 public class 메소드_4_구현 {
-
+	// 사용자로부터 정수를 받는다(입력)
+	// static => 자동저장
+	static int userInput()
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.print("단 입력(2~9):");
+		//int dan=scan.nextInt();
+		return scan.nextInt();
+	}
+	
+	// 받은 정수에 해당되는 구구단 출력
+	/* static String gugudan(int dan) 
+	{
+		String result=dan+"단\n";
+		for(int i=1;i<=9;i++) {
+			result+=dan+"*"+i+"="+dan*i+"\n";
+		}
+		return result;
+	} */
+	// 위의 메소드와 같은결과를 가져오지만, 다른 방법 => 메소드 내에서 출력
+	static void gugudan(int dan)
+	{
+		System.out.println(dan+"단");
+		for(int i=1;i<=9;i++) {
+			System.out.println(dan+"*"+i+"="+dan*i);
+		}
+	}
+	
+	// 조립기
+	static void process() 
+	{
+		int dan=userInput();
+		//String gu=gugudan(dan);
+		//System.out.println(gu);
+		gugudan(dan);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		process(); // 객체 지향 프로그램 => 기능별로 나눠서 코딩 => 조립기 => main에서는 조립기만 호출 => 다른 파일에서 재사용가능
+		
+		
 	}
 
 }
---
+++ Chapter04_메소드_1/src/메소드_4_재사용.java
@@ -0,0 +1,10 @@
+
+public class 메소드_4_재사용 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		메소드_4_구현.process(); // 메소드의 장점 : 재사용!! (객체지향 프로그램)
+							  // main에 작성시 재사용 불가하므로 따로 조립기를 만들어서 재사용이 가능하게끔 함
+	}
+
+}
