3a7338379bfb56fe69e27b4f78789116965e9848
parkna98 <pkpphn@naver.com>
2023-10-17 21:43:36 +0900

    hh

--- Chapter04_배열연습문제/src/문제1.java
+++ Chapter04_배열연습문제/src/문제1.java
@@ -23,7 +23,7 @@ public class 문제1 {
 		}
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(rank));*/
-		
+		 
 		/*int[] kor=new int[5];
 		int[] rank=new int[5];
 		// 난수 생성
