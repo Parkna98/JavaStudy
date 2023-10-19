360491ae58440024a2849bbc5c2c01f82c451f4b
Parkna98 <pkpphn@naver.com>
2023-10-19 12:52:25 +0900

    11

---
+++ Chapter08_예외처리_1/src/com/sist/exception/MainClass_2.java
@@ -0,0 +1,23 @@
+package com.sist.exception;
+// 예외처리의 기본(267page)
+/*
+ * 		가장 많이 사용된다 => 예외복구
+ * 		try~catch~finally
+ * 		1) 형식
+ * 			try
+ * 			{
+ *				에러없이 실행이 가능한 소스
+ *				// main ==> 정상수행하는 문장 => 개발자 실수, 사용자의 입력 
+ * 			}catch(Exception ex) => Exception: 모든에러를 잡아주는 최상위클래스
+ * 			{
+ * 				예외복구 ==> 다시 입력 요청 => 다음 문장으로 이동
+ * 			} ==> catch는 예상되는 예외를 따로 처리 => 여러개 사용이 가능
+ */
+public class MainClass_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
